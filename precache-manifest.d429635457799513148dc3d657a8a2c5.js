9accc7122543f382a19326d1ac66f0bee82e907a
Muyeen Ansar <muyeen.ansar@sl.iec.local>
2019-11-10 07:13:15 +0530

    Updates

---
+++ precache-manifest.d429635457799513148dc3d657a8a2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b6a2ce36ca2e35ff02e65fdbbbe0be",
+    "url": "/GoBot/index.html"
+  },
+  {
+    "revision": "6288db53358a7149c8fc",
+    "url": "/GoBot/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "c43b42cc7932aa3f11ca",
+    "url": "/GoBot/static/css/main.dc270af5.chunk.css"
+  },
+  {
+    "revision": "6288db53358a7149c8fc",
+    "url": "/GoBot/static/js/2.7ae72a90.chunk.js"
+  },
+  {
+    "revision": "c43b42cc7932aa3f11ca",
+    "url": "/GoBot/static/js/main.b715f634.chunk.js"
+  },
+  {
+    "revision": "619fe2bf53b6831c0eab",
+    "url": "/GoBot/static/js/runtime-main.eb2cea40.js"
+  }
+]);
